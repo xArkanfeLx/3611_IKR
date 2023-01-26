9cf8a2fc0ddd1be0ff33b65ab7c0b9f737ff6bf9
xArkanfeLx <Arkanfel92@gmail.com>
2023-01-26 20:43:37 +0300

    Создан массив в программе и рандомно заполняется строками с разной длинной

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,19 @@
 ﻿Console.Clear();
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Алгоритм формирует массив строк с длинной 3 из сгенерированного массива строк");
+
+int arrayLength = new Random().Next(3,20);
+string[] array = new string[arrayLength];
+
+void GenerateStringValuesToArray(string[] arr){
+    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (int i=0; i<arr.Length;i++) {
+     arr[i] = new string(Enumerable.Repeat(chars, new Random().Next(1,10))
+        .Select(s => s[new Random().Next(0,chars.Length)]).ToArray());
+    }
+}
+
+GenerateStringValuesToArray(array);
+
+Console.WriteLine($"Длинна массива равна {arrayLength}");
+Console.WriteLine($"Сгенерированный массив [{string.Join(" ,", array)}]");
